ec789abbb7333c9ee5cc6bfd34826aae3040046d
Roger Chao <rogman@gmail.com>
2020-04-02 20:23:56 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "AWS_Secret_Engine_Output" {
   value = <<README
 
-EC2 Instance IP: ${module.instance.public_ip}
+EC2 Instance IP: ${aws_instance.instance.public_ip}
 
-ssh -i awskey.pem ec2-user@${module.instance.public_ip}
+ssh -i awskey.pem ec2-user@${aws_instance.instance.public_ip}
 
 AWS Keys used by Terraform to provision Instances:
-Access Key: ${module.instance.access_key}
-Secret Key: ${module.instance.secret_key}
+Access Key: ${aws_instance.instance.access_key}
+Secret Key: ${aws_instance.instance.secret_key}
 README
 }
