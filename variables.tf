631ada680d58618847ae921001811ce0c69ab24c
Roger Chao <rogman@gmail.com>
2020-04-02 20:17:00 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,9 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-2"
-}
-
-variable "aws_profile" {
-  description = "AWS profile used for provider connection"
-  default     = "default"
-}
-
-variable "instance_type" {
-  description = "type of EC2 instance to provision."
-  default     = "t2.micro"
-}
-
-variable "name" {
-  description = "name to pass to Name tag"
-  default     = "rogmanster"
-}
-
-variable "owner" {
-  description = "Name to pass to the Owner tag"
-  default     = "rchao"
-}
-
-variable "ttl" {
-  description = "Hours until instances are reaped by N.E.P.T.R"
-  default     = "3"
-}
-
-variable "description" {
-  default     = "test instance"
-}
+variable "instance_type" {}
+variable "name" {}
+variable "owner" {}
+variable "ttl" {}
+variable "aws_region" {}
+variable "aws_profile" {}
+variable "role_name" {}
+variable "depends_on_trigger" { type = any}
+variable "path" {}
