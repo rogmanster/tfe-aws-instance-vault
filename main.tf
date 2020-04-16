16620bf0b4c1edc00ce2d5727fbe5aa8e676f47a
rogman <rogman@0.hsd1.ca.comcast.net>
2020-04-16 12:08:25 -0700

    test

--- main.tf
+++ main.tf
@@ -64,7 +64,6 @@ resource "aws_instance" "instance" {
     Name        = var.name
     TTL         = var.ttl
     Owner       = var.owner
-    Description = "This branch updated v5 - test"
+    Description = "This branch updated v6"
   }
 }
-
